fc2c92666f52737d019b84571ab228d7386bf713
Shreya111111 <shreyanalawade882@gmail.com>
2023-04-20 21:59:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import './App.css'
+const App = () => {
+  
+ 
+  const commands = [
+    {
+      command: 'reset',
+      callback: ({ resetTranscript }) => {
+        resetTranscript();  }
+      
+    },
+    {
+      command: 'clear',
+      callback: ({ resetTranscript }) => resetTranscript()
+    },
+    {
+      command: 'open *',
+      callback: (site) => { window.open('http://' + site) }
+    },
+    {
+      command: 'increase text size',
+      callback: () => {document.getElementById('cont').style.fontSize = '30px'}
+    },
+    {
+      command: 'decrease text size',
+      callback: () => {document.getElementById('cont').style.fontSize = '10px'}
+    },
+    {
+      command: 'change text colour to *',
+      callback: (color) => {document.getElementById('cont').style.color =  color}
+    }
+  ]
 
-function App() {
+  SpeechRecognition.startListening({ continuous: true, language:'en-IN' })
+  const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands })
+
+  
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='navbar'>
+        <h1>Type something to write..</h1>
+        <h2>Some of the commands to use in the speech recognition..</h2>
+        <ul>
+          <li>Open *: To open the browser</li>
+          <li>Reset : To reset the transcript</li>
+          <li>Clear : To clear the transcript</li>
+          <li>Increase text size : To increase text size of the transcript</li>
+          <li>Decrease text size : To decrease text size of the transcript</li>
+          <li>Change text colour to * :To change text colour to * of the transcript</li>
+        </ul>
+      </div>
+      <div id='cont'>
+        {transcript}
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
